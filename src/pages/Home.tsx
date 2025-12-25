abc26f4856e8fa8d1378570f24dc1fa7f23f6115
hgulizoda <hgulizodait@gmail.com>
2025-12-25 19:17:43 +0500

    text input added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import UserCard from "../components/userCard";
 import API from "../utils/api";
 import type { IUserCard } from "../types/tUserCard";
-import { Grid } from "@mantine/core";
+import { Button, Flex, Grid, TextInput } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 type UsersArray = IUserCard[];
 
 const Home = () => {
+  const [search, setSearch] = useState<string>("");
+  const [filtered, setFiltered] = useState<UsersArray>([]);
+
   const { data } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
@@ -15,16 +20,43 @@ const Home = () => {
     },
   });
 
-  console.log(data);
+  useEffect(() => {
+    const newArray = data?.filter(
+      (user) =>
+        user.name.toLowerCase().includes(search?.toLowerCase()) ||
+        user.username.toLowerCase().includes(search?.toLowerCase())
+    );
+    setFiltered(newArray);
+  }, [search, data]);
 
   return (
     <div className="container">
+      <Flex>
+        <Flex gap={10}>
+          <TextInput
+            mb={20}
+            value={search ? search : ""}
+            onChange={(e) => setSearch(e.target.value)}
+            w={300}
+            placeholder="search with username or name"
+          />
+          <Button bg={"gray"}>
+            <IconSearch />
+          </Button>
+        </Flex>
+      </Flex>
       <Grid>
-        {data?.map((user: IUserCard) => (
-          <Grid.Col span={3}>
-            <UserCard key={user.id} {...user} />
-          </Grid.Col>
-        ))}
+        {search
+          ? filtered?.map((user: IUserCard) => (
+              <Grid.Col span={3} key={user.id}>
+                <UserCard {...user} />
+              </Grid.Col>
+            ))
+          : data?.map((user: IUserCard) => (
+              <Grid.Col span={3} key={user.id}>
+                <UserCard {...user} />
+              </Grid.Col>
+            ))}
       </Grid>
     </div>
   );
